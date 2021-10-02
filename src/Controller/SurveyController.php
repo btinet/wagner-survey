d8cdebefb94ec5d4635efdd6823f505ea68ea891
Benjamin Voigt <benjamin.voigt@vapita.de>
2021-10-02 07:45:12 +0200

    surveys updated.

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -26,64 +26,71 @@ class SurveyController extends AbstractController
      */
     public function show(Survey $survey, Request $request, EntityManagerInterface $manager): Response
     {
-
-        /*
-        $form = $this->createFormBuilder(new Survey());
-        foreach($survey->getSurveyLines() as $line){
-            if($line->getAnswers()){
-                $form->add($line->getTitle(),ChoiceType::class,[
-                    'choices' => $line->getAnswers()
-                ]);
-            } else {
-                $form->add('',TextType::class);
-            }
-        }
-        $form->getForm();
-        */
-
-        if($request->isMethod('POST')) {
-            //dd($request->request->all());
-            if ($survey->getCode() == $request->request->get('Code')) {
-
-                $form = $request->request->all();
-                /*
-                $previousResult = $this->getDoctrine()->getRepository(SurveyResult::class)
-                    ->getNextLineNumber();
-                if($previousResult->getLineNumber()){
-                    $lineNumber = $previousResult->getLineNumber();
-                    $lineNumber++;
+        $now = new \DateTime('now');
+        if ($survey->getStart() <= $now and $now <= $survey->getEnd()) {
+            /*
+            $form = $this->createFormBuilder(new Survey());
+            foreach($survey->getSurveyLines() as $line){
+                if($line->getAnswers()){
+                    $form->add($line->getTitle(),ChoiceType::class,[
+                        'choices' => $line->getAnswers()
+                    ]);
                 } else {
-                    $lineNumber = 1;
+                    $form->add('',TextType::class);
                 }
-                */
-                $_form = [];
-                foreach ($form as $question => $answer) {
-                    $_form[$question] = $answer;
-                }
-                try {
-                    $surveyResult = new SurveyResult();
-                    /*
-                    $surveyResult->setQuestion($question);
-                    $surveyResult->setAnswer($answer);
-                    $surveyResult->setLineNumber($lineNumber);
-                    */
-                    $surveyResult->setContent($_form);
-
-                    $manager->persist($surveyResult);
-                } catch (ORMException $e) {
-
-                }
-                $manager->flush();
-                $this->addFlash('success', 'Umfrage erfolgreich abgeschlossen. Vielen Dank!');
-                return $this->redirectToRoute('app_index');
-            } else {
-                $this->addFlash('warning', 'Der Code stimmt nicht überein!');
             }
-        }
+            $form->getForm();
+            */
 
-        return $this->render('survey/show.html.twig', [
-            'survey' => $survey,
-        ]);
+            if ($request->isMethod('POST')) {
+                //dd($request->request->all());
+
+                if ($survey->getCode() == $request->request->get('Code') and $survey->getStart() <= $now and $now <= $survey->getEnd()) {
+
+                    $form = $request->request->all();
+                    /*
+                    $previousResult = $this->getDoctrine()->getRepository(SurveyResult::class)
+                        ->getNextLineNumber();
+                    if($previousResult->getLineNumber()){
+                        $lineNumber = $previousResult->getLineNumber();
+                        $lineNumber++;
+                    } else {
+                        $lineNumber = 1;
+                    }
+                    */
+                    $_form = [];
+                    foreach ($form as $question => $answer) {
+                        $_form[$question] = $answer;
+                    }
+                    try {
+                        $surveyResult = new SurveyResult();
+                        /*
+                        $surveyResult->setQuestion($question);
+                        $surveyResult->setAnswer($answer);
+                        $surveyResult->setLineNumber($lineNumber);
+                        */
+                        $surveyResult->setContent($_form);
+
+                        $manager->persist($surveyResult);
+                    } catch (ORMException $e) {
+
+                    }
+                    $manager->flush();
+                    $this->addFlash('success', 'Umfrage erfolgreich abgeschlossen. Vielen Dank!');
+                    return $this->redirectToRoute('app_index');
+                } else {
+                    $this->addFlash('warning', 'Der Code stimmt nicht überein!');
+                }
+            }
+
+            return $this->render('survey/show.html.twig', [
+                'survey' => $survey,
+            ]);
+
+        } else {
+            $this->addFlash('warning', 'Diese Umfrage ist abgelaufen!');
+            return $this->redirectToRoute('app_index');
+        }
     }
 
 
